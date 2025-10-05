fcb49288ab622471dad1741228c02a22b26c51b4
mertcaliskan01 <mcaliskanmert@gmail.com>
2025-10-06 00:02:39 +0300

    Update default port to 5001 and improve HTML structure in index template

--- app.py
+++ app.py
@@ -89,5 +89,5 @@ def outputFile(filename):
     return send_from_directory(app.config['OUTPUT_FOLDER'], filename)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port)
